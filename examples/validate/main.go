0a5632ab7f6df9aa35c5b001ffd35129194d6445
Seth Erickson <sr.erickson@gmail.com>
2023-06-29 15:25:39 -0700

    move validate program to 'examples'

---
+++ examples/validate/main.go
@@ -0,0 +1,47 @@
+package main
+
+// validates an OCFL object or storage root on the local filesystem.
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/srerickson/ocfl"
+	"github.com/srerickson/ocfl/ocflv1"
+	"golang.org/x/exp/slog"
+)
+
+var (
+	isStore bool
+	name    string
+	logger  = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelWarn}))
+)
+
+func main() {
+	flag.BoolVar(&isStore, "store", false, "validate path as an OCFL storage root")
+	flag.Parse()
+	name = flag.Arg(0)
+	if name == "" {
+		logger.Error("missing required path argument")
+		os.Exit(1)
+	}
+	if isStore {
+		logger = logger.With("storage_root", name)
+	} else {
+		logger = logger.With("object_path", name)
+	}
+	if err := validate(name, isStore, logger); err != nil {
+		os.Exit(1)
+	}
+}
+
+func validate(root string, isStore bool, logger *slog.Logger) error {
+	fsys := ocfl.NewFS(os.DirFS(root))
+	ctx := context.Background()
+	if isStore {
+		result := ocflv1.ValidateStore(ctx, fsys, ".", ocflv1.ValidationLogger(logger))
+		return result.Err()
+	}
+	result := ocflv1.ValidateStore(ctx, fsys, ".", ocflv1.ValidationLogger(logger))
+	return result.Err()
+}
