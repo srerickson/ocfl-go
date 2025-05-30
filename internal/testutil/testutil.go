4b06d8656943c0dd9c32c6d13560738c03120f40
Seth Erickson <sr.erickson@gmail.com>
2025-05-30 21:07:12 +0000

    test helper

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -2,10 +2,12 @@ package testutil
 
 import (
 	"errors"
+	"os"
 	"slices"
 	"testing"
 
 	"github.com/srerickson/ocfl-go"
+	"github.com/srerickson/ocfl-go/fs/local"
 )
 
 // ErrorsIncludeOCFLCode checks that errs includes at least one
@@ -22,4 +24,19 @@ func ErrorsIncludeOCFLCode(t *testing.T, ocflCode string, errs ...error) {
 	if !slices.Contains(foundCodes, ocflCode) {
 		t.Errorf("OCFL validation code %q not in found validation codes %v", ocflCode, foundCodes)
 	}
+}
+
+// make a temporary local.FS for writing with contents
+// of srcdir copied into it
+func TmpLocalFS(t *testing.T, srcDir string) *local.FS {
+	t.Helper()
+	tmpDir := t.TempDir()
+	if err := os.CopyFS(tmpDir, os.DirFS(srcDir)); err != nil {
+		t.Fatal(err)
+	}
+	fsys, err := local.NewFS(tmpDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return fsys
 }
